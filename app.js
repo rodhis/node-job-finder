1c8266bffa13eb4c0ff56aed5752413133b5990b
Rodhis <rodrigonoliva@gmail.com>
2024-04-17 21:20:31 -0300

    added db tables and model for sequelize

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
+const db = require('./db/connection')
 
 const PORT = 3000
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log(`O express está rodando na porta ${PORT}`);
 })
+
+//db connection
+db.authenticate()
+.then(() => {
+    console.log('Conectado com sucesso ao banco de dados');
+})
+.catch(err => {
+    console.log('Erro na conexão com o banco de dados:', err);
+})
+
+
+//routes
 
 app.get('/', (req, res) => {
     res.send('Está funcionando siiimm')
