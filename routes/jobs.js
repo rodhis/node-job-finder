ef85ccf9c4202d79aa8890a1efca339bde476b50
rodhis <rodrigonoliva@gmail.com>
2024-05-08 17:31:53 -0300

    added job view page

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,16 +2,25 @@ const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
 
-// rota de teste
-router.get("/test", (req, res) => {
-  res.send("deu certo");
-});
+// job details
+router.get("/view/:id", (req, res) =>
+  Job.findOne({
+    where: { id: req.params.id },
+  })
+    .then((job) => {
+      res.render("view", {
+        job,
+      });
+    })
+    .catch((err) => console.log(err))
+);
 
+// job adding page
 router.get("/add", (req, res) => {
   res.render("add");
 });
 
-//post
+// POST request route of job adding page
 router.post("/add", (req, res) => {
   let { title, salary, company, description, email, new_job } = req.body;
 
